ffc485ce40b3afc46f11be2990b1ad4e55cf1f5b
Ashish Tyagi <ashishtyagi10@gmail.com>
2021-08-23 07:02:44 -0400

    added rest

--- rust-rest-api/src/main.rs
+++ rust-rest-api/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(plugin)]
+#[macro_use] extern crate rocket;
+
+use rocket::{Rocket, Build};
+
+#[get("/<name>/<age>")]
+fn hello(name: String, age: u8) -> String {
+    format!("Hello, {} year old named {}!", age, name)
+}
+
+#[get("/world")]              // <- route attribute
+fn world() -> &'static str {  // <- request handler
+    "hello, world!"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build().mount("/", routes![hello])
+        .mount("/hi", routes![world])
+        .mount("/hello", routes![world])
 }
